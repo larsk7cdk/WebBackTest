ced21f1489b731bccaa48f7e82c0199672a120d8
lars@k7c.dk <lars@k7c.dk>
2019-04-25 18:27:00 +0200

    added integration test

---
+++ WebBackTest.IntegrationTest/Controllers/TodoControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebBackTest.IntegrationTest.Controllers
+{
+    public class TodoControllerTest : IClassFixture<CustomWebApplicationFactory<web.Startup>>
+    {
+        private readonly CustomWebApplicationFactory<web.Startup> _factory;
+        private readonly HttpClient _client;
+
+        public TodoControllerTest(CustomWebApplicationFactory<web.Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_IndexToDo_Schould_ReturnIndexWithSuccess()
+        {
+            // Arrange
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+
+            //Act
+            
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
---
+++ WebBackTest.IntegrationTest/CustomWebApplicationFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebBackTest.web.Infrastructure.Data;
+
+namespace WebBackTest.IntegrationTest
+{
+    public class CustomWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var serviceProvider = new ServiceCollection()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .BuildServiceProvider();
+
+                services.AddDbContext<TodoDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                    options.UseInternalServiceProvider(serviceProvider);
+                });
+
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<TodoDbContext>();
+                    var logger = scopedServices.GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                    db.Database.EnsureCreated();
+
+                    try
+                    {
+                        // Seed the database with test data.
+                        //Utilities.InitializeDbForTests(db);
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "An error occurred seeding the database with test messages. Error: {ex.Message}");
+                    }
+                }
+            });
+        }
+    }
+}
